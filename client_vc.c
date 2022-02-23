fdb51bf19e43d56d31f285a5d88e6482389cd027
JSaurusRex <jessetonckens@gmail.com>
2022-02-23 20:19:02 +0100

    actually made ip changable by argument

--- client_vc.c
+++ client_vc.c
@@ -85,7 +85,7 @@ int main(int argc, char** argv)
 
 	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
 	servaddr.sin_port = htons(PORT);
 
 	// connect the client socket to server socket
